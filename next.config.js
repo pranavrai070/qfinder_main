3cb8d0458b46ef3800e7e55dd2669845b2457cce
pranavrai070 <pranavrai070@gmail.com>
2023-12-20 12:54:00 +0530

    next config revert to original

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-const { BLOG_URL } = process.env;
-
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: `/:path*`,
-      },
-      {
-        source: "/blog",
-        destination: `${BLOG_URL}/blog`,
-      },
-      {
-        source: "/blog/:path*",
-        destination: `${BLOG_URL}/blog/:path*`,
-      },
-    ];
-  },
-};
+module.exports = nextConfig
